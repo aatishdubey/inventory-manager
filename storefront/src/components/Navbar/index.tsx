39de0a45dafc16e7a04f50a6db0d2de35593c58e
Aatish Dubey <atsh_dubey@live.com>
2023-04-25 23:14:38 +0545

    created navbar component

---
+++ storefront/src/components/Navbar/index.tsx
@@ -0,0 +1,12 @@
+export const Navbar = () => {
+  return (
+    <div className="container sticky top-0 mx-auto p-4">
+      <div className="flex justify-between items-center">
+        <div className="text-2xl font-bold">Storefront</div>
+        <div className="flex items-center">
+          <div>Logout</div>
+        </div>
+      </div>
+    </div>
+  )
+}
