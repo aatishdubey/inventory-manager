3d42bee8668c0ccf02b5c9c4ee67ec79ffb315b3
Aatish Dubey <atsh_dubey@live.com>
2023-04-29 19:32:39 +0545

    check path instead of auth for showing logout

--- inventory-fe/src/components/Navbar/index.tsx
+++ inventory-fe/src/components/Navbar/index.tsx
@@ -1,13 +1,10 @@
 import { useEffect, useState } from "react"
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { APP_NAME } from "src/config";
-import { useCognitoAuth } from "src/hooks/useCognitoAuth";
 
 export const Navbar = () => {
-
-  const { userAuth } = useCognitoAuth();
-
   const [isTransparent, setIsTransparent] = useState(true);
+  const location = useLocation();
 
   const toggleTransparency = () => {
     if(window.scrollY > 100) {
@@ -32,7 +29,7 @@ export const Navbar = () => {
         <div className="flex justify-between items-center">
           <div className="text-2xl font-bold">{APP_NAME}</div>
           <div className="flex items-center">
-            {userAuth.accessToken && (
+            {location.pathname === "/" && (
               <div className="mr-4">
                 <Link to="/logout">Logout</Link>
               </div>
