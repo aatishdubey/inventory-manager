e390fca363b08f52d3e0616b528c5040b79c0c95
Aatish Dubey <atsh_dubey@live.com>
2023-04-29 19:15:57 +0545

    added user pool id and client id to config

--- inventory-fe/src/config.ts
+++ inventory-fe/src/config.ts
@@ -1,2 +1,5 @@
 export const API_BASE_URL = "https://eibn5s9583.execute-api.ap-south-1.amazonaws.com/";
 export const APP_NAME = "Serverless Inventory";
+
+export const USER_POOL_ID = "ap-south-1_7qMKR8qY5";
+export const CLIENT_ID = '10cj58s3hamc8v79ls4kkitv0v';
