3903fe4348edccfa25fe4bb0ed19dbaa37598e79
Aatish Dubey <atsh_dubey@live.com>
2023-04-29 19:15:40 +0545

    added logout and login routes

--- inventory-fe/src/App.tsx
+++ inventory-fe/src/App.tsx
@@ -1,29 +1,26 @@
-import {
-  QueryClientProvider,
-} from '@tanstack/react-query'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
-import { BaseLayout } from '@components/BaseLayout';
-import Products from './pages/Products';
-import { Login } from './pages/Login';
-import { queryClient } from './api';
+import { QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BaseLayout } from "@components/BaseLayout";
+import Products from "./pages/Products";
+import { Login } from "./pages/Login";
+import { queryClient } from "./api";
+import { Logout } from "./pages/Logout";
+import { ProtectedRoute } from "@components/ProtectedRoute";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BaseLayout>
-          <Router>
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/" element={<Products />} />
-            </Routes>
-          </Router>
-      </BaseLayout>
+      <Router>
+        <BaseLayout>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/logout" element={<ProtectedRoute><Logout /></ProtectedRoute>} />
+            <Route path="*" element={<ProtectedRoute><Products /></ProtectedRoute>} />
+          </Routes>
+        </BaseLayout>
+      </Router>
     </QueryClientProvider>
-  )
+  );
 }
 
-export default App
+export default App;
